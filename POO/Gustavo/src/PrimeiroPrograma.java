6c465d178e5ccca2ea64e97860f2832ef1e8398b
gustavo.sobrinho <gustavo-ssobrinho@hotmail.com>
2019-08-28 08:01:58 -0300

    Organizacao de pastas

--- POO/Gustavo/src/PrimeiroPrograma.java
+++
@@ -1,8 +0,0 @@
-/*Made By = Gustavo dos Santos Sobrinho*/
-
-public class PrimeiroPrograma {
-	
-	public static void main(String[] args) {
-		System.out.println("Primeiro Programa, vulgo, Hello World!");
-	}
-}
